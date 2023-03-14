ba76216b6bfa985f62b140745da3c595b07ea948
suyeon-ai <kr.suyeon.kim@gmail.com>
2023-03-15 01:52:44 +0900

    test: testing store models

--- store/tests.py
+++ store/tests.py
@@ -1,3 +1,34 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
 
-# Create your tests here.
+from .models import Category, Product
+
+
+class TestCategoriesModel(TestCase):
+    
+    def setUp(self):
+        self.test_data = Category.objects.create(name='TOUR MERCH', slug='tour-merch')
+    
+    # Category class testing
+    def test_category_model_entry(self):
+        data = self.test_data
+        self.assertTrue(isinstance(data, Category))
+        self.assertEqual(str(data), 'TOUR MERCH')  
+    
+
+class TestProductsModel(TestCase):
+    
+    def setUp(self):
+        Category.objects.create(id=4, name='TOUR MERCH', slug='tour-merch')
+        User.objects.create(id=2, username='test1')
+        self.test_data = Product.objects.create(product_name='4 cuts photo', price='10000', description='testing',
+                                                product_image='None', detail_image='None', slug='4-cuts-photo', in_stock=1, is_active=1,
+                                                category_id=4, created_by_id=2)
+    
+    # Product class testing
+    def test_product_model_entry(self):
+        data = self.test_data
+        self.assertTrue(isinstance(data, Product))
+        self.assertEqual(str(data), '4 cuts photo')   
+
+    
