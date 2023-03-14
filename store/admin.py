2c034e9536359e7a1790873cb4850169db120092
suyeon-ai <kr.suyeon.kim@gmail.com>
2023-03-14 21:33:13 +0900

    feat: add categories and products management as an admin

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Category, Product
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+    prepopulates_fields = {'slug': ('name',)}
+    
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['category', 'product_name', 'price', 'description', 'slug',
+                    'in_stock', 'is_active', 'created', 'updated']
+    list_filter = ['category', 'in_stock', 'is_active']
+    list_editable = ['price', 'in_stock', 'is_active']
+    prepopulated_fields = {'slug': ('product_name', )}
