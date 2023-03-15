df997c314d23b3986d1c4cbbec6e7f17d3b10ba9
suyeon-ai <kr.suyeon.kim@gmail.com>
2023-03-16 06:05:01 +0900

    design: create base header and main page with bootstrap

--- core/settings.py
+++ core/settings.py
@@ -54,6 +54,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.humanize',
     'store',
 ]
 
@@ -80,6 +81,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'store.views.categories',
             ],
         },
     },
